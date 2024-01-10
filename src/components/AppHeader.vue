49221aa6ff4f41392b6e798afef91170e8ce1251
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-01-10 13:35:26 +0100

    Header layout setup

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,8 +6,22 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <section class="header_container bg-dark p-2">
+        <div class="container d-flex justify-content-between">
+            <h2 class="text-danger m-0">BoolFlix</h2>
+            <div class="d-flex">
+                <input placeholder="Found a Film/TV Series" type="text">
+                <button type="button" class="btn btn-danger ms-2">Search</button>
+            </div>
+        </div>
+    </section>
+</template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
+
+input::placeholder {
+    text-align: center;
+}
 </style>
