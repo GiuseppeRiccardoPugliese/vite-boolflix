9c72d9b2306677b656ad81b28f1110e6e8686fd0
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-01-10 12:23:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+//Importo axios
+import axios from 'axios';
+
+//Importo lo store
+import { store } from './store';
+
+//Importo i componenti figli
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      store,
+    }
+  }
+}
+
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+</style>
